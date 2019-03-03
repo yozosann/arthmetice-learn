de76334344454b6b4cc99c168629f1ccc7886ec8
tianjie <yangtianjie@meituan.com>
2019-03-03 08:13:27 +0800

    [done]

--- leetcode/17. Letter Combinations of a Phone Number.js
+++ leetcode/17. Letter Combinations of a Phone Number.js
@@ -2,15 +2,27 @@
  * @param {string} digits
  * @return {string[]}
  */
+let results = [];
+let digitsArr = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
+
 var letterCombinations = function(digits) {
-    let digitsMap = {
-        2: 'abc',
-        3: 'def',
-        4: 'ghi',
-        5: 'jkl',
-        6: 'mno',
-        7: 'pqrs',
-        8: 'tuv',
-        9: 'wxyz',
-    };
+    if(!digits) return '';
+    getRes('', 0, digits);
+
+    console.log(results);
+    return results;
 };
+
+function getRes(cur, index, digits) {
+    // console.log(results, cur, index, digits, digits.length);
+    if(index === digits.length) {
+        return results.push(cur);
+    }
+
+    let str = digits[index];
+    for(let i = 0; i < digitsArr[str].length; i++) {
+        getRes(cur + digitsArr[str][i], index + 1, digits);
+    }
+}
+
+letterCombinations('233');
