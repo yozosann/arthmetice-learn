5b113efc4b4a0e5828b6fbcd36b7ed3926202dd7
tianjie <yangtianjie@meituan.com>
2019-03-03 23:32:11 +0800

    [done] done 25

--- leetcode/25. Reverse Nodes in k-Group.js
+++ leetcode/25. Reverse Nodes in k-Group.js
@@ -10,6 +10,52 @@
  * @param {number} k
  * @return {ListNode}
  */
-var reverseKGroup = function(head, k) {
-    
+var reverseKGroup = function (head, k) {
+    if (!head) return null;
+    let cur = head,
+        i = 0;
+    head = new ListNode(-1);
+    head.next = cur;
+    cur = head;
+
+    while(!(i % k) && cur) {
+        let temp = [], j = 0, tempHead = cur;
+        while(j < k && cur) {
+            cur = cur.next;
+            temp[j] = cur;
+            j++;
+            i++;
+        }
+        if(temp.filter(i => i).length === k) {
+            tempHead.next = temp[k-1];
+            for(let m = 0; m < k; m++) {
+                temp[m].next = m - 1 >= 0 ? temp[m - 1] : temp[k - 1].next;
+            }
+            cur = temp[0];
+        }
+    }
+
+    console.log(head.next);
+    return head.next;
 };
+
+reverseKGroup(creatList([1,2,3,4,5]),2);
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+function creatList(arr) {
+    if (!arr.length) return null;
+    let head = new ListNode(arr[0]);
+    let cur = head;
+    arr = arr.slice(1);
+
+    arr.reduce((pre, cur) => {
+        let temp = new ListNode(cur);
+        pre.next = temp;
+        return temp;
+    }, cur)
+    return head;
+}
